bee60d805c34a2ba0ef2b04bd7651cdc4b9c62c7
andres <a20191450@pucp.edu.pe>
2024-04-26 19:55:48 -0500

    a

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -1,12 +1,29 @@
 package com.example.laboratorio3.entity;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMax;
 import java.util.Date;
 
-//COMPLETAR
+@Entity
+@Getter
+@Setter
+@Table(name="employees")
 public class Employees {
-
-           //COMPLETAR
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private int id;
+    @Column(nullable = false)
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private String phone_number;
+    private String hire_date;
+    private double salary;
+    private double commission_pct;
 }
