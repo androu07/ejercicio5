44babfd238fcff63fbb7b232ef2d750711c11bc4
Ana Paula <anapaula.hachi@hotmail.com>
2024-04-26 20:55:42 -0500

    second

--- src/main/java/com/example/laboratorio3/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio3/controller/HistoryController.java
@@ -1,9 +1,37 @@
 package com.example.laboratorio3.controller;
 
 
-//COMPLETAR
-public class HistoryController {
-//COMPLETAR
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@Controller
+public class HistoryController {
+
+    final
+    EmployeesRepository employeesRepository;
+
+    public HistoryController(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
+    }
+    @GetMapping("/historial")
+    public String historialEmpleados(){
+
+        return "history/historial";
+    }
+
+    @PostMapping("/BuscarHistorial")
+    public String buscarHistorial(@RequestParam("searchField") String searchField,
+                                      Model model){
+        List<Employees> employeesList = employeesRepository.findByFirst_nameOrLast_name(searchField);
+        model.addAttribute("listaEmployees",employeesList);
+        return "history/historial";
+    }
 
 }
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -5,8 +5,11 @@ import com.example.laboratorio3.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
+    List<Employees> findByFirst_nameOrLast_name(String palabra);
 
 }
